ab182929ceb6419f125832044bb651f6ee5be0dc
sahilwashiwale <sahil.washiwale7557@gmail.com>
2025-11-19 20:22:10 +0530

    relational activities

---
+++ Basic_JS/Relational_opts.js
@@ -0,0 +1,23 @@
+// relational operator=> > , < , >= , <= , == , != , ===
+// always o/p of relational opt is boolean value like True or false
+// console.log(100>10);
+
+// console.log(100<10);
+
+// console.log(10>=10);
+
+// console.log(10!=10);
+
+// console.log(10==10);
+
+let num1 = 12;
+let num2 = "12";
+ console.log(num1==num2); // give o/p is true bcoz only check value not data type 
+
+ console.log(num1+3);
+ console.log(num2+1);
+ 
+ console.log(num1===num2); // give o/p is false bcoz check value as well as data type 
+ 
+ 
+ 
