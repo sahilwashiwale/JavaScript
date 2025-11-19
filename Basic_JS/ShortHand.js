83586a115e390f18a09b65cf0e3fb6d287e33b28
sahilwashiwale <sahil.washiwale7557@gmail.com>
2025-11-19 20:26:46 +0530

    shorthand properties

---
+++ Basic_JS/ShortHand.js
@@ -0,0 +1,11 @@
+// '='=> assignment oprator assign value to a variable
+// do all arithmatic opration with shorthand property 
+let num = 154;
+num+=6;
+console.log(num);
+
+let num2 = 100;
+num2-=5;
+console.log(num2);
+
+
