048cd9b33f52c50ab8fa9fdaa014aab96bd48300
sahilwashiwale <sahil.washiwale7557@gmail.com>
2025-11-19 20:25:23 +0530

    special activities

---
+++ Basic_JS/Special_opt.js
@@ -0,0 +1,20 @@
+//pre-increment and post-increment operators
+// its unary oprators bcoz do operation in single variable like num 
+
+
+let num=45;
+num++;// post incre=> do opration and after increase the value
+++num;// before increase the value and do opration 
+console.log(num++);
+console.log(num);
+
+//pre-decrement and post-decrement operators
+
+let num2 = 100;
+num2--;
+--num2;
+--num2;
+console.log(num2--);
+console.log(num2);
+
+
