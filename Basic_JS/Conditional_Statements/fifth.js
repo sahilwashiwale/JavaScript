0216c386e46a7f30f15551647ed808351b6e8422
sahilwashiwale <sahil.washiwale7557@gmail.com>
2025-11-21 13:15:28 +0530

    functions

---
+++ Basic_JS/Conditional_Statements/fifth.js
@@ -0,0 +1,13 @@
+/**
+ * function => named reusable code block
+ * u can use it multiple times as per ur need
+ * hosting the function is possible
+ */
+
+Demo();
+function Demo()
+{
+    console.log("hii");
+    
+}
+Demo();
