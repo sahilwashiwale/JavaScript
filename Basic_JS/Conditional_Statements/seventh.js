7e9f1ad8a9513aba8d6e14a1b3c1eddc8ac6ea0f
sahilwashiwale <sahil.washiwale7557@gmail.com>
2025-11-23 13:05:49 +0530

    practice code

---
+++ Basic_JS/Conditional_Statements/seventh.js
@@ -0,0 +1,22 @@
+
+function ADD()
+{
+    let num1 = 20;
+    let num2 = 30;
+    console.log(num1+num2)
+}
+
+function evenodd()
+{
+var num = 21;
+if(num%2==0)
+{
+    console.log(num,"even");
+}
+else{
+    console.log(num,"odd");
+}
+}
+
+evenodd();
+ADD();
