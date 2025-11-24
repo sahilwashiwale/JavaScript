f0c107c8c7e2553d2ac1ae900c1ddc6090a4721a
sahilwashiwale <sahil.washiwale7557@gmail.com>
2025-11-24 18:41:55 +0530

    default value example

---
+++ Basic_JS/Conditional_Statements/default_values.js
@@ -0,0 +1,17 @@
+/**
+ * ES2020 => features 
+ */
+add();
+function add(v)// not neceesary to give let var const u only give parameter
+{
+   // console.log("hii"+v); // o/p => hiiundefined bcoz v is undefined
+    //console.log(123+v); // o/p => NaN
+    
+}
+add2("abs");
+function add2(v)
+{
+    console.log("hii"+v);
+    
+}
+add2("sahil");
