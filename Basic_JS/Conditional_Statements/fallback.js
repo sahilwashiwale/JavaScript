d4091458d11c17298f4f7fcea3eb38890d16ecaa
sahilwashiwale <sahil.washiwale7557@gmail.com>
2025-11-24 18:40:03 +0530

    fall back examples

---
+++ Basic_JS/Conditional_Statements/fallback.js
@@ -0,0 +1,24 @@
+/**
+ * falsey value fallback
+ * 
+ * nullish coealscion
+ * 
+ * default values(ES2020)
+ * 
+ * 0, "" ,  false , undefined , null    
+ */
+
+ let result = false;
+ let congratulation = result || " u r best"; // fall back 
+
+ console.log(congratulation);
+ 
+ let result1 = undefined;
+ let congratulation1 = result1 || " u r best"; // fall back 
+
+ console.log(congratulation1);
+
+ let result3 = 100;
+ let congratulation3 = undefined || result3; // fall back 
+
+ console.log(congratulation3+100);
