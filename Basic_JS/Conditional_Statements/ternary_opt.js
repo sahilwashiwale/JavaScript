72559cf68ec49172a9fe1dea7a8ff36df8395ac5
sahilwashiwale <sahil.washiwale7557@gmail.com>
2025-11-24 18:38:59 +0530

    ternary operator example

---
+++ Basic_JS/Conditional_Statements/ternary_opt.js
@@ -0,0 +1,21 @@
+/**
+ * if else => ternary operator =>three oprands =>if-else or ternary
+ */
+
+// if(num>35)
+// {
+//     console.log("pass");
+// }
+// else{
+//     console.log("fail");
+    
+// }
+let num=34;
+let result;//undefined
+let result1;
+result = num>35 ? "pass":"fail"; // ternary opeartor
+console.log(result);
+
+
+result1 = num>35 ? "C": num>50 ? "B":"A"; //nested ternary operator
+console.log(result1);
