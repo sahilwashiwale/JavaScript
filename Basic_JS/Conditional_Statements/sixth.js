807d4548d63ca2c9c3c45deea6ac57bbe0db774f
sahilwashiwale <sahil.washiwale7557@gmail.com>
2025-11-25 10:28:22 +0530

    functions in JS

--- Basic_JS/Conditional_Statements/sixth.js
+++ Basic_JS/Conditional_Statements/sixth.js
@@ -4,7 +4,7 @@ Demo();
 // console.log(car,car2,);
 function Demo()
 {
-    let car = "ms";  // not go outside from function
+    let car = "mercedes";  // not go outside from function
     const car2="scoda";  // not go outside from function
     var car3 = "bmw"; // not go outside from function
     console.log(car,car2,car3); //bcoz let var const is function scope
