e1508cacaeb50a0772dbd550938c612101145253
sahilwashiwale <sahil.washiwale7557@gmail.com>
2025-11-25 10:31:01 +0530

    example in variable

--- Basic_JS/sec.js
+++ Basic_JS/sec.js
@@ -6,6 +6,6 @@
 // age = 15;
 // console.log(age); //print latest value=>15 and this cannot redeclare but u reassign new val
 
-// const pi = 12;
-// pi = 15;
-// console.log(pi); // cannot redeclare as well as reassign it must be initaialize when declaration
+const pi = 12;
+pi = 15;
+console.log(pi); // cannot redeclare as well as reassign it must be initaialize when declaration
