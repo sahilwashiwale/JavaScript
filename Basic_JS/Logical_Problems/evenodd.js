e15adf68eec5273fb75a89b300882421e36d719d
sahilwashiwale <sahil.washiwale7557@gmail.com>
2025-11-27 21:36:00 +0530

    evenodd example

---
+++ Basic_JS/Logical_Problems/evenodd.js
@@ -0,0 +1,17 @@
+
+let num = 12237;
+
+// if(num%2==0)
+// {
+//     console.log("even");
+    
+// }
+// else{
+//     console.log("odd");
+    
+// }
+
+let result = num%2==0 ?"even":"odd"; // using ternary operator
+console.log(result);
+
+
