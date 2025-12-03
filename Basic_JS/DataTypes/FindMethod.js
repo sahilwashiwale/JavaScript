ea7aac949cdfee31e7361f50a42589d665f43266
sahilwashiwale <sahil.washiwale7557@gmail.com>
2025-12-03 12:29:57 +0530

    array method

---
+++ Basic_JS/DataTypes/FindMethod.js
@@ -0,0 +1,13 @@
+
+let arr=[10,20,30,40,50];
+/**
+ * higher order component/function
+ */
+
+// console.log(arr.find((i)=>(i>13)) || "not in array");
+console.log(arr.find((i)=>{return i>13}) || "not in array"); // annonymos arrow function
+// function checkvalue(i)
+// {
+//     return i>46;
+// }
+
