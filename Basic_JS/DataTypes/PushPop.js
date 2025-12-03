8c67450f261bac575fdccdf933dbbc815e8bc759
sahilwashiwale <sahil.washiwale7557@gmail.com>
2025-12-03 12:32:09 +0530

    array method

---
+++ Basic_JS/DataTypes/PushPop.js
@@ -0,0 +1,25 @@
+
+let arr=[10,20,30,40,50];
+
+//Appends new elements to the end of an array, and returns the new length of the array. modifying method
+
+// console.log(arr.push(60,70,80)); // 8 => length
+
+//  console.log(arr);
+ /*[
+  10, 20, 30, 40,
+  50, 60, 70, 80 
+]
+*/
+
+//Removes the last element from an array and returns it.
+//  If the array is empty, undefined is returned and the array is not modified.
+
+console.log(arr.pop()); // 80
+console.log(arr);/*
+[
+  10, 20, 30, 40,
+  50, 60, 70     
+]
+  */
+
