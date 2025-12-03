2f413edb47a47988a2b37d2185e004d6d9fad50e
sahilwashiwale <sahil.washiwale7557@gmail.com>
2025-12-03 12:32:43 +0530

    array example

---
+++ Basic_JS/DataTypes/number.js
@@ -0,0 +1,11 @@
+let num=1234;
+console.log(typeof num);//number
+
+let num2='sahil';
+console.log(typeof num2);// string
+
+let num1 = "12";
+console.log(num1-12); //0 only subtraction can convert string into number
+console.log(num1+12);//1212
+console.log(num1*12);//144
+console.log(num1/12);//1
