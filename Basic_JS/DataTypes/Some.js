e3a1eec4d6a48cab8ae0d2a79cad714c6e0bf01c
sahilwashiwale <sahil.washiwale7557@gmail.com>
2025-12-04 10:42:36 +0530

    array method

---
+++ Basic_JS/DataTypes/Some.js
@@ -0,0 +1,21 @@
+ 
+
+ let arr = [12,43,23,41,65,11];
+/**
+ * some => true/false logical "OR" => even if the single element is passing the given condition
+ * u will get the answer true
+ * 
+ */
+ function greaterthan50(i)
+ {
+          return i>50;
+ }
+
+console.log(arr.some(greaterthan50));
+
+/**
+ * every => each element of given array must pass given condition 
+ * it works logical "AND" n return value accordingly
+ */
+
+console.log(arr.every(greaterthan50));
