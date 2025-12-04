c8e8115c32e7fdf8fabd28dd66934ddb3a3edbdf
sahilwashiwale <sahil.washiwale7557@gmail.com>
2025-12-04 10:42:13 +0530

    array method

---
+++ Basic_JS/DataTypes/Fill.js
@@ -0,0 +1,6 @@
+
+let arr=[10,23,45,6,78,765,8];
+//fill also modifying method
+arr.fill('sahil',2,5);//        2   ,    3   ,    4             not take 5
+console.log(arr);//[ 10, 23, 'sahil', 'sahil', 'sahil', 765, 8 ]
+
