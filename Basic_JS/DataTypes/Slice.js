1786c883ab9ebcea7035f04f7a0452381a885c81
sahilwashiwale <sahil.washiwale7557@gmail.com>
2025-12-08 18:35:59 +0530

    array methods

---
+++ Basic_JS/DataTypes/Slice.js
@@ -0,0 +1,11 @@
+
+let arr=['a','b','c','d'];
+/**
+ *  start and end number of index
+ * it is ude to create the slice  of given array
+ * original as it is after changing
+ */
+
+console.log(arr.slice(1,3)); //0,1,2 not 3 exclude
+
+console.log(arr);
