c6b105d5bda296c8e005c943f903e5269760d9d6
sahilwashiwale <sahil.washiwale7557@gmail.com>
2025-12-08 18:36:14 +0530

    array methods

---
+++ Basic_JS/DataTypes/Splice.js
@@ -0,0 +1,10 @@
+
+let arr = ['mango','banana','apple','orange'];
+arr.splice(1,2,'x','y');//start_index,delete_count,elements add at start_index
+// console.log(arr);
+
+let arr2 = ['v','d','d','b'];
+// to find => v=> abc <=b
+// modify original array
+arr2.splice(1,2,'a','b','c');
+console.log(arr2);
