9517c721d5c6b0e269cd09fc49ef31313fae6aef
sahilwashiwale <sahil.washiwale7557@gmail.com>
2025-12-09 10:02:37 +0530

    array methods

---
+++ Basic_JS/DataTypes/Includes.js
@@ -0,0 +1,11 @@
+
+let arr = [12,3,4,5,6,66,7,8,];
+
+/**
+ * includes => will return the boolean value 
+ * it accept two parameters => (element_to_search , index_from_to_start_the_search)
+ */
+
+console.log(arr.includes(3,4));// 3 is number and 4 is index_from_to_start_the_search
+
+console.log(arr.includes(66));
