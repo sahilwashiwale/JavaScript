6407965c541aa642725baedd354b87a56282fe7e
sahilwashiwale <sahil.washiwale7557@gmail.com>
2025-12-10 15:28:20 +0530

    array method example

---
+++ Basic_JS/Logical_Problems/practice10.js
@@ -0,0 +1,13 @@
+// to find the number is greater than 5
+
+let arr=[3,21,45,3,4,5,1,6,7,8,9,4,3];
+let find=[];
+
+for(i=0;i<arr.length;i++)
+{
+    if(arr[i]>5)
+    {
+        find.push(arr[i])
+    }
+}
+console.log(find);
