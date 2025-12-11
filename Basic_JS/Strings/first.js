70e712d4d8fc6786100f51cb779f916f2993ec01
sahilwashiwale <sahil.washiwale7557@gmail.com>
2025-12-11 23:20:38 +0530

    String example

---
+++ Basic_JS/Strings/first.js
@@ -0,0 +1,17 @@
+
+// string => combination / collection of chracters.
+// It is primitive data type in JS.
+// string is 0 based index in JS.
+
+// there are two approches to create in JS
+// a) String Literal.
+// b) String() constructor.
+
+// a) String Literal =>
+
+    let str=("hello");
+    let str2=('hello');
+    let name=('sahil');
+    let str3=(`washiwale ${name} `); // template literal only use this not('' n "")
+    console.log(str,str2,str3);
+    
