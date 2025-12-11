bd41a01612edbe7eff72fc3a839cfab1cb21f659
sahilwashiwale <sahil.washiwale7557@gmail.com>
2025-12-11 23:18:21 +0530

    String method

---
+++ Basic_JS/Strings/Methods/At.js
@@ -0,0 +1,8 @@
+
+let str=(' sahil washiwale');
+
+// At() => it accept positive as well as negative indices. 
+// negative index will count from back.
+
+console.log(str.at(-3)); // a
+console.log(str.at(3)); //h
