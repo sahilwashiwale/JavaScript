ec8069696aac42166f5f54917e7059de64ffcaa5
sahilwashiwale <sahil.washiwale7557@gmail.com>
2025-12-11 23:22:00 +0530

    String method

---
+++ Basic_JS/Strings/Methods/concat.js
@@ -0,0 +1,11 @@
+
+let str="sahil";
+
+//Returns a string that contains the concatenation of two or more strings.
+//not modifying method
+
+let str2=str.concat('washiwale');
+
+console.log(str2);//sahilwashiwale
+
+
