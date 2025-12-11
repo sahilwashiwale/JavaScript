771466b51a62d867beb19eff301047c8e8248ad7
sahilwashiwale <sahil.washiwale7557@gmail.com>
2025-12-11 23:20:56 +0530

    String example

---
+++ Basic_JS/Strings/second.js
@@ -0,0 +1,9 @@
+
+// b) String() Constructor =>
+
+    let str = new String('hello');
+    console.log(str); //[String: 'hello'] => to print object bcoz use 'new' keyword
+    
+    let str2 = String('world');
+    console.log(str2); //world => to print primitive value.
+    
