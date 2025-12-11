978418e0ec5a80b928db02713505e9cf89482708
sahilwashiwale <sahil.washiwale7557@gmail.com>
2025-12-11 23:22:28 +0530

    String method

---
+++ Basic_JS/Strings/Methods/Repeat.js
@@ -0,0 +1,6 @@
+
+let str=('abcd');
+//Returns a String value that is made from count copies appended together.
+//  If count is 0, the empty string is returned.
+console.log(str.repeat(2));// abcdabcd => repeat given string 2 times bcoz u given 2
+console.log(str.repeat(0));//count is 0,so empty string is returned.
