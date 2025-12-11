559a5dbe795f9579bcf0dfc6831c0ca5ccd8fa7b
sahilwashiwale <sahil.washiwale7557@gmail.com>
2025-12-11 23:18:58 +0530

    String method

---
+++ Basic_JS/Strings/Methods/CharAt.js
@@ -0,0 +1,10 @@
+
+let str=('sahil washiwale');
+
+// charAt() => Returns the character at the specified index
+//             starts from o index n only takes positive index number.
+
+ console.log(str.charAt(9)); // h 
+
+ 
+ 
