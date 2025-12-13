fb5d8f41e3396dd237fee0552ef32ebf49c43fd2
sahilwashiwale <sahil.washiwale7557@gmail.com>
2025-12-13 17:45:37 +0530

    string methods

---
+++ Basic_JS/Strings/Methods/endsWith.js
@@ -0,0 +1,10 @@
+
+let str=(' sahil washiwale ')
+
+//.endsWith() => match the sequence of end character n give op as boolean value
+// its alse searching method
+
+
+console.log(str.endsWith(' ')); // true bcoz its ending with space
+
+console.log(str.endsWith('e'));// false bcoz its ending with space not e
