2cd64b6c741f60cd2417b023f19c0013c9f2f92c
sahilwashiwale <sahil.washiwale7557@gmail.com>
2025-12-13 17:46:39 +0530

    string methods

---
+++ Basic_JS/Strings/Methods/Substring.js
@@ -0,0 +1,5 @@
+let str=('abcdefgh');
+// same as slice method
+console.log(str.substring(1,5)); // 1 to 4 print 5 is exclude
+console.log(str.substr(1,5)); // 1 is how to start no n 5 is after 1 (1 include)
+// take 5 elements 
