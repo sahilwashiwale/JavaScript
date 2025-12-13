1e6e4611fb73892f180778c4fba04f7cf8a034e2
sahilwashiwale <sahil.washiwale7557@gmail.com>
2025-12-13 17:43:28 +0530

    string methods

---
+++ Basic_JS/Strings/Methods/LocaleCompare.js
@@ -0,0 +1,6 @@
+
+let str=('aa');
+let str2=('ab');
+
+console.log(str.localeCompare(str2)); //bcoz str Is less than str2(str<str2)=> -1
+console.log(str2.localeCompare(str)); // bcoz str2 > str so => 1
