f1a715cb840e96c4a5a62c258f91f038c79c92e5
sahilwashiwale <sahil.washiwale7557@gmail.com>
2025-12-13 17:46:08 +0530

    string methods

---
+++ Basic_JS/Strings/Methods/ValueOf.js
@@ -0,0 +1,10 @@
+
+let str = new String('abcde');
+
+console.log(str);//[String: 'abcde'] => object 
+
+//Returns the primitive value of the specified object.
+
+console.log(str.valueOf());//abcde => primitive
+
+
