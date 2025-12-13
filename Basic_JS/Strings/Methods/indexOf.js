08a4a4d1e0c27c403436ca5e44961e0f226f4823
sahilwashiwale <sahil.washiwale7557@gmail.com>
2025-12-13 17:44:07 +0530

    string methods

---
+++ Basic_JS/Strings/Methods/indexOf.js
@@ -0,0 +1,12 @@
+let str=('i am the best i am the best i am the best');
+
+//.indexOf() => Returns the index number of first occurrence of a substring.
+            //  its also searching opeartion
+            // if start index n end index not present any chracter it gives -1
+
+console.log(str.indexOf('best',20));//23// bcoz start index 20 to onwords n 
+//                                         best value is present on 20 to onwords
+
+console.log(str.indexOf('e')); // 7 bcoz from start 'e' present on 7 no index
+
+
