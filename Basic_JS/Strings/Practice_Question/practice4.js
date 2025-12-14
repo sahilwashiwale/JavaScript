646b93c08ddfa4cdabbad437dc97cbd44340ca63
sahilwashiwale <sahil.washiwale7557@gmail.com>
2025-12-14 22:50:49 +0530

    practice examples

---
+++ Basic_JS/Strings/Practice_Question/practice4.js
@@ -0,0 +1,14 @@
+// find first and last index of is
+// i/p=> ("this is island")
+//o/p=> 2,5
+let str= ("this is island");
+
+// console.log("first",str.indexOf("is"));
+// console.log("last",str.lastIndexOf("is",7));
+
+// console.log(str.length);
+
+//find convert string ABC into A*B*C
+let str2="ABC";
+
+console.log(str2[0].concat("*",str2[1].concat("*"),str2[2]));//A*B*C
