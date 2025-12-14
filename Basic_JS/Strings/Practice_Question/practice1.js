f214fb4b030371f330244d44bdb09ad2445528a2
sahilwashiwale <sahil.washiwale7557@gmail.com>
2025-12-14 22:50:02 +0530

    practice examples

---
+++ Basic_JS/Strings/Practice_Question/practice1.js
@@ -0,0 +1,15 @@
+
+//find second last character of string using at()
+// i/p=> 'javascript' o/p=> 'p' 
+
+let str=('JavaScript');
+//console.log(str.at(-2));
+
+// check if word ends with 'ing' using endsWith()
+// i/p=>'learning'
+
+let str2=('learning');
+let str3=('learn');
+
+console.log(str2.endsWith('ing'));//true
+console.log(str3.endsWith('ing'));//false
