00be2e46eadf2ce92adec365eb34553e2ac3462c
sahilwashiwale <sahil.washiwale7557@gmail.com>
2025-12-14 22:50:59 +0530

    practice examples

---
+++ Basic_JS/Strings/Practice_Question/practice5.js
@@ -0,0 +1,18 @@
+
+// find middle char using charat()
+// i/p=> hello o/p =>l
+let str=('hello');
+//console.log(str.charAt(str.length/2));//ll
+
+// check if last digit is even or odd
+let str2=('12345');
+let str3=('12344');
+if(str3.at(-1)%2==0)
+{
+    console.log("even");
+    
+}
+else{
+    console.log("odd");
+    
+}
