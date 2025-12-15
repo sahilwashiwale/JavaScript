a77fbb70f414a5fffc3229f6f37c21f766c2d3a1
sahilwashiwale <sahil.washiwale7557@gmail.com>
2025-12-15 23:28:48 +0530

    string methods

---
+++ Basic_JS/Strings/Methods/StartsWith.js
@@ -0,0 +1,10 @@
+
+let str=('abcdef');
+//Returns true if the sequence of elements of searchStringo
+//  is the same as the corresponding elements 
+//  at starting position. Otherwise returns false.
+
+console.log(str.startsWith('ab'));//true
+console.log(str.startsWith('ab',1));//its searching from 1 index n 1 index 
+// present 'b' not 'a' so give false
+
