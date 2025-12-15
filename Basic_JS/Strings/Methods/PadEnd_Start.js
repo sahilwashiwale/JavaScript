5e8057015af23a029d0ca3388dab39a3b748676f
sahilwashiwale <sahil.washiwale7557@gmail.com>
2025-12-15 23:27:57 +0530

    string methods

---
+++ Basic_JS/Strings/Methods/PadEnd_Start.js
@@ -0,0 +1,11 @@
+
+let str=('abcd');
+
+//Pads or fill the current string with a given string
+console.log(str.padEnd(6,'&'));//curent 4 and given 6 
+// so fill 4+2 2 is given as '&' at the end position
+ //abcd&&
+
+console.log(str.padStart(6,'&'));//curent 4 and given 6 
+// so fill 4+2 2 is given as '&' at the start position
+ //abcd&&
