57941b792a71e1895acc92c61baff885f0d40816
sahilwashiwale <sahil.washiwale7557@gmail.com>
2025-12-15 23:29:51 +0530

    string methods

---
+++ Basic_JS/Strings/Methods/lastIndexOf.js
@@ -0,0 +1,12 @@
+
+let str=('i am the best i am the best i am the best');
+
+//.lastIndexOf() => Returns the index number of last occurrence of a substring
+                //  its starts from last index 
+                //  its also searching opeartion
+                // its searching from given index to backwords(desending order)
+            
+
+console.log(str.lastIndexOf('the',40)); //33//
+                          // bcoz searching from 40 index to backword direction n 
+                          // 'the' value present in 33 index 
