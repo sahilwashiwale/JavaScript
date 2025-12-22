f69689462f8620f718a26a5a123c9c7433e72d04
sahilwashiwale <sahil.washiwale7557@gmail.com>
2025-12-22 11:19:32 +0530

    String Methods

---
+++ Basic_JS/Strings/Methods/Match.js
@@ -0,0 +1,20 @@
+
+let str=('i Am p22324une I am242 pune PunE i24214 Am aM p434213unE ');
+
+//console.log(str.match(/a/i));
+//console.log(str.match(/a/ig));
+//console.log(str.match(/\d+/g));
+//console.log(str.match(/[1-3]+/g));
+//console.log(str.match(/[^1-3]+/g));
+
+
+
+/**
+ * i => case insensative 
+ * ig => case insensative + global(multiple occurance)
+ * \ => ascep char 
+ * /\d/g => search each number present in array
+ * /\d+/g => search for the numbers as whole 
+ * /[1-3]+/g => including range 1 to 3 means take 1 2 3
+ * /[^1-3]+/g => without including range 1 to 3 means take 1 2 3
+ */
